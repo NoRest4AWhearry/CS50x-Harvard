383c978b2d19263dd362135f099ed256d98e4039
unknown <awhearry@gmail.com>
2015-08-19 12:01:26 -0400

    fix code styling

--- pset1/hello.c
+++ pset1/hello.c
@@ -1,6 +1,17 @@
+/***********************************************************
+** Allen Whearry Jr.
+*
+** instagram.com/NoRest4AWhearry
+*
+** Hello:
+*
+** This is a solution of "Hello" from
+** pset1 of CS50x Harvard.
+************************************************************/
 #include <stdio.h>
 
 int main(void)
 {
+    // Prints Hello
     printf("hello, world\n");
 }
--- pset1/mario.c
+++ pset1/mario.c
@@ -1,3 +1,13 @@
+/***********************************************************
+** Allen Whearry Jr.
+*
+** instagram.com/NoRest4AWhearry
+*
+** Mario:
+*
+** This is a solution of "Mario" from
+** pset1 of CS50x Harvard.
+************************************************************/
 #include <stdio.h>
 #include <cs50.h>
 
@@ -9,24 +19,27 @@ int main(void)
         printf("Height:");
         height = GetInt();
         if (height == 0)
+        {
             return 0;
+        }
     }
     while(height < 1 || height >= 24); 
     
     // Create correct number of rows
     for(int row = 0; row < height; row++) 
-	{
-		// Create corret number of spaces
-        for(int space = 0; space < height-row-1; space++)
+    {
+    // Create corret number of spaces
+        for(int space = 0; space < height - row - 1; space++)
         {
             printf("%s", " ");
         }
-        // Create correct number of hashes.
-		for(int hash = 0; hash < row+2; hash++)
-		{
-			printf("%s", "#");
+        // Create correct number of hashes.       
+        for(int hash = 0; hash < row + 2; hash++)
+        {
+            printf("%s", "#");
 		}
-		printf("\n");
+		
+        printf("\n");
 	}	
     return 0;
 }
